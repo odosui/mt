aeca488d0b00bfb0b9dc7995fb50ede4a40d7099
Ianis Triandafilov <janis.sci@gmail.com>
2025-12-30 09:27:09 +0100

    fix side panel transition

--- client/src/components/SidePanel.tsx
+++ client/src/components/SidePanel.tsx
@@ -29,6 +29,7 @@ const SidePanel = ({
           initial={{ opacity: 0, x: 300 }}
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: 300, transition: { duration: 0.2 } }}
+          transition={{ type: 'tween', duration: 0.25, ease: 'easeOut' }}
         >
           <div className={className}>
             <div className="util-side-panel-toggle" onClick={toggleVisible}>
